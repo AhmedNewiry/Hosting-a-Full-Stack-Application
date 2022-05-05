c1de43bf407bfec857f3d855c1611219bc42bb47
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-05-05 18:36:38 +0200

    third

--- udagram-frontend/bin/deploy.sh
+++ udagram-frontend/bin/deploy.sh
@@ -1,1 +1,1 @@
-aws s3 cp --recursive --acl public-read ./www s3://ahmed-udacity/
+aws s3 cp --recursive --acl public-read ./www s3://udagram-udacity43/
--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "Udagram",
-  apiHost: "http://Udagramserver-env.eba-c2epdsz8.us-east-1.elasticbeanstalk.com/api/v0",
+  apiHost: "http://Udagramserver-env.eba-bpp53agj.us-east-1.elasticbeanstalk.com/api/v0",
 };
 
 /*
