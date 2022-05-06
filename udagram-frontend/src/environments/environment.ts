7f728bfdce1d8f0c453aff6753b54602a8aad683
AhmedNewiry <ahmedabdalfttah@outlook.com>
2022-05-06 02:31:14 +0200

    third

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://Udagramserver-env.eba-bpp53agj.us-east-1.elasticbeanstalk.com/api/v0'
 };
 
 /*
